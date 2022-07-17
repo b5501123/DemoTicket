f589c0d3642cdc6c377a56c8d5d4183b95d72bd0
ben <ben.liu@arionlabs.com.tw>
2022-07-17 12:44:21 +0800

    fix table

--- Demo.API/SQL/DemoDataSeed.sql
+++ Demo.API/SQL/DemoDataSeed.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE `Demo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `Demo`.`User` (
@@ -8,18 +9,19 @@ CREATE TABLE `Demo`.`User` (
   `Role` INT NOT NULL,
   PRIMARY KEY (`UserID`));
   
-  CREATE TABLE `GoDemo`.`Ticket` (
-  `TicketID` INT NOT NULL AUTO_INCREMENT,
-  `Type` INT NOT NULL,
-  `Severity` INT NOT NULL,
-  `Summary` VARCHAR(45) NOT NULL,
-  `Description` VARCHAR(500) NOT NULL,
-  `IsResolved` TINYINT(1) NOT NULL,
-  `IsDel` TINYINT(1) NOT NULL,
-  `ResolvedTime` DATETIME NULL,
-  `CreateBy` VARCHAR(45) NOT NULL,
-  `CreateTime` DATETIME NOT NULL,
-  PRIMARY KEY (`TicketID`));
+  CREATE TABLE  `Demo`.`Ticket` (
+  `TicketID` int NOT NULL AUTO_INCREMENT,
+  `Type` int NOT NULL,
+  `Severity` int NOT NULL,
+  `Summary` varchar(50) NOT NULL,
+  `Description` varchar(500) NOT NULL,
+  `IsResolved` tinyint(1) NOT NULL,
+  `IsDel` tinyint(1) NOT NULL,
+  `ResolvedTime` datetime DEFAULT NULL,
+  `CreateBy` varchar(45) NOT NULL,
+  `CreateTime` datetime NOT NULL,
+  PRIMARY KEY (`TicketID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `Demo`.`User` (`UserID`, `Account`, `Name`, `Password`, `Role`) VALUES ('1', 'RD01', 'RD01', '123456', '1');
 INSERT INTO `Demo`.`User` (`UserID`, `Account`, `Name`, `Password`, `Role`) VALUES ('2', 'QA01', 'QA01', '123456', '2');
